953f6ea93e50aac4cac2d55224cd1943045a737e
smakidomuhoreca-hash <smaki.domu.horeca@gmail.com>
2026-05-11 22:04:37 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
 import os
 
-# Вставте сюди токен від @BotFather
-"BOT_TOKEN" = 8575401574:AAGw3jgfphaN_J7CUkGySSjWMM7exnsJjnc
+BOT_TOKEN = os.getenv("BOT_TOKEN", "ВАШ_ТОКЕН_ТУТ")
 
-# Telegram ID адміністратора (власника)
-# Щоб дізнатися свій ID — напишіть боту @userinfobot
-ADMIN_IDS = [368848319],  # замініть на ваш реальний Telegram ID
-
-
+ADMIN_IDS = [
+    368848319,
+]
